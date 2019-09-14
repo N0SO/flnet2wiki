0f3e7f7e86557759036d31685642a0d99b7561d1
Michael Heitmann, N0SO <mheitmann@n0so.net>
2019-09-14 00:40:46 -0500

    Changes for V1.0.0

--- flnet2wiki_gui.py
+++ flnet2wiki_gui.py
@@ -16,7 +16,7 @@ else:
 from flNettoWiki import FLNettoWiki
 import os.path
 
-VERSION = '0.1.0'
+VERSION = '1.0.0'
 
 class wikiExtentions(FLNettoWiki):
     def __init__(self):
@@ -40,17 +40,17 @@ class wikiWin(Frame):
         pythonversion = sys.version.splitlines()
         infotext = \
         'FLNET2WIKI_GUI - Version ' + VERSION + '\n' + \
-        'Utilities to convert FLNET files to Wiki TABLE format.\n' \
+        'Utilities to convert FLNET files to Wiki TABLE format.\n'\
         + 'Python ' + pythonversion[0]
         showinfo('FLNET2WIKI_GUI', infotext)
         
     def OpenFile(self):
         print ("Open FLLog File!")
         fileName = askopenfilename(title = "Select FLNet Logfile:",
-                                      filetypes=[("LOG files","*.log"),
-                                                 ("CSV files","*.csv"),
-                                                 ("Text files","*.txt"),
-                                                 ("All Files","*.*")])
+                              filetypes=[("LOG files","*.log"),
+                                         ("CSV files","*.csv"),
+                                         ("Text files","*.txt"),
+                                         ("All Files","*.*")])
         if os.path.isfile(fileName):
             print('File name selected: %s'%(fileName))
             self.wikistuff.fileName = fileName
@@ -65,25 +65,30 @@ class wikiWin(Frame):
         print ('Convert to Wiki format...')
         temp = self.wikistuff.convert_to_wiki(self.wikistuff.logData)
         wikiText = self.wikistuff.make_wiki_entry( \
-                        temp, whichnet = None)
-        self.fillLogTextfromData(wikiText, self.LogText, clearWin=True)
+                        temp, whichnet = self.netType.get())
+        self.fillLogTextfromData(wikiText, self.LogText, 
+                                                clearWin=True)
 
     def SaveWikiTable(self):
         print ('Convert to Wiki Table format...')
         temp = self.wikistuff.convert_to_wiki_table(self.wikistuff.logData)
         #print('===>WikiText:\n%s'%(temp))
         wikiText = self.wikistuff.make_wiki_entry( \
-                                temp, whichnet = None)
+                                temp, 
+                                whichnet = self.netType.get())
 
-        self.fillLogTextfromData(wikiText, self.LogText, clearWin=True)
+        self.fillLogTextfromData(wikiText, self.LogText, 
+                                                clearWin=True)
 
-    def fillLogTextfromData(self, Data, textWindow, clearWin = False):
+    def fillLogTextfromData(self, Data, textWindow, 
+                                             clearWin = False):
         if (clearWin):
             textWindow.delete(1.0, END)
         for line in Data:
             textWindow.insert(END, line.strip()+'\n')
 
-    def fillLogTextfromFile(self, filename, textWindow, clearWin = False):
+    def fillLogTextfromFile(self, filename, textWindow, 
+                                            clearWin = False):
         if (clearWin):
             textWindow.delete(1.0, END)
         try: 
@@ -94,8 +99,12 @@ class wikiWin(Frame):
            retText = ('Could not read file: '%(fName))
         return retText
   
+    def net_options(self):
+        print('Net Type %s selected...'%(self.netType.get()))
+
     def init_window(self):
         self.root = Tk()
+        self.netType = StringVar(None, 'Sunday')
         self.S = Scrollbar(self.root)
         self.LogText = Text(self.root, height=10, width=120)
         self.S.pack(side=RIGHT, fill=Y)
@@ -108,11 +117,35 @@ class wikiWin(Frame):
         self.root.config(menu=menu)
         self.filemenu = Menu(menu)
         menu.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="Open FLNet File", command=self.OpenFile)
+        self.filemenu.add_command(label="Open FLNet File", 
+                                        command=self.OpenFile)
         self.filemenu.add_separator()
-        self.filemenu.add_command(label="Convert to Wiki...", command=self.SaveWiki, state="disabled")
-        self.filemenu.add_command(label="Convert to Wiki Table...", command=self.SaveWikiTable, state="disabled")
-        self.filemenu.add_command(label="Exit", command=self.root.quit)
+        self.filemenu.add_command(label="Convert to Wiki...", 
+                                        command=self.SaveWiki, 
+                                              state="disabled")
+        self.filemenu.add_command(label="Convert to Wiki Table...",
+                      command=self.SaveWikiTable, state="disabled")
+        self.filemenu.add_command(label="Exit", 
+                                            command=self.root.quit)
+    
+        self.optionsmenu = Menu(menu)
+        menu.add_cascade(label='Options', menu=self.optionsmenu)
+        self.optionsmenu.add_radiobutton(label = \
+                                        'Sunday Night 2M Net',
+                                        variable = self.netType,
+                                        value = 'Sunday',
+                                        command = self.net_options)
+        self.optionsmenu.add_radiobutton(label = \
+                                        'EOC VHF Check Net', 
+                                        variable = self.netType,
+                                        value = 'VEOC',
+                                        command = self.net_options)
+        self.optionsmenu.add_radiobutton(label = \
+                                        'EOC UHF Check Net',
+                                        variable = self.netType,
+                                        value = 'UEOC',
+                                        command = self.net_options)
+    
     
         helpmenu = Menu(menu)
         menu.add_cascade(label="Help", menu=helpmenu)
